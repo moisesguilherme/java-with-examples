d9688b06c75fb0e6fb336932b5a48b56e556ebe6
Moisés Guilherme <moisescomp@gmail.com>
2024-06-29 16:27:45 -0300

    8. Stream rows form text file, sort, filter and print

--- JavaStreams/streams/src/JavaStreams.java
+++ JavaStreams/streams/src/JavaStreams.java
@@ -1,11 +1,11 @@
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.IntStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 public class JavaStreams {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         /*
         // 1. Integer Stream
         IntStream
@@ -60,7 +60,7 @@ public class JavaStreams {
                 .average()
                 .ifPresent(System.out::println);
         // 44.0
-        */
+
 
         // 7. Stream from List, filter and print
         List<String> people = Arrays.asList("Alex", "Bruno", "Sara" , "Amanda", "Hans");
@@ -69,6 +69,15 @@ public class JavaStreams {
             .map(String::toLowerCase)
             .filter(x -> x.startsWith("a"))
             .forEach(System.out::println);
+        */
+
+        // 8. Stream rows form text file, sort, filter and print
+        Stream<String> bands = Files.lines(Paths.get("bands.txt"));
+        bands
+           .sorted()
+           .filter(x -> x.length() > 13)
+           .forEach(System.out::println);
+        bands.close();
 
    }
 
